45ccba9d76df8239494f719842cb09fed75fee88
Samuel Marks <samuelmarks@gmail.com>
2016-07-19 16:12:22 +1000

    All tests now pass concurrently! :D

--- test/shared_tests.js
+++ test/shared_tests.js
@@ -3,6 +3,11 @@ var async = require('async');
 function tearDownConnections(connections, cb) {
     return connections ? async.parallel(Object.keys(connections).map(function (connection) {
         return connections[connection]._adapter.teardown;
-    }), cb) : cb();
+    }), function () {
+        Object.keys(connections).forEach(function (connection) {
+            connections[connection]._adapter.connections.delete(connection);
+        });
+        cb();
+    }) : cb();
 }
 exports.tearDownConnections = tearDownConnections;
--- test/shared_tests.ts
+++ test/shared_tests.ts
@@ -1,8 +1,13 @@
-import * as async from 'async';
 import {Connection} from 'waterline';
+import * as async from 'async';
 
 export function tearDownConnections(connections: Connection[], cb) {
     return connections ? async.parallel(Object.keys(connections).map(
         connection => connections[connection]._adapter.teardown
-    ), cb) : cb()
+    ), () => {
+        Object.keys(connections).forEach(connection => {
+            connections[connection]._adapter.connections.delete(connection);
+        });
+        cb();
+    }) : cb();
 }
